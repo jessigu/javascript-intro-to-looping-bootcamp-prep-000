a3fcc3f95677a9c2632b00b802793c53b8265c8e
Jess Guttenberg <jessigu@gmail.com>
2018-08-07 17:12:08 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,7 @@
+function forLoop(array) {
+    for (let i = 0; i < 25; i++) {
+      console.log(`I am  + ((i === 1) ? 1 : ${i}) + strange loop + ((if i === 1) ? '.' : 's.')`);
+        return array;
+    }
+}
+
