d9c0f4d048847f5e26a723a8474cc95011170f14
Jess Guttenberg <jessigu@gmail.com>
2018-08-07 17:26:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,11 +2,9 @@ function forLoop(array) {
     for (let i = 0; i < 25; i++) {
       if (i === 1) {
         array.push(`I am 1 strange loop.`);
-      } else if (i=== 0) {
-        array.push(`I am ${i} strange loop.`)
       } else {
-        array.push(`I am ${i} strange loops.`)
-      }
+        array.push(`I am ${i} strange loop${i===0 ? ' : 's`}.`)
+      } 
       return array;
     }
 }
